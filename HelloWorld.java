0252a0d18e2d6a9647cb2bac2cf7d68b4879df77
Samuel Rodrigues Mendes <samuelrodriguesmendes96@gmail.com>
2025-09-05 10:20:42 -0300

    branch sampplier

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
     public static void main(String args[])
         System.out.println("Hello World!");
-        Sytem.out.println("Engenharia de Software");
+        System.out.println("Engenharia de Software");
+        System.out.println("Atividade");
+
 }
